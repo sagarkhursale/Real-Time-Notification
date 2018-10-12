47c33482bf253e01b2bc06294684bd4478610c6b
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-13 03:33:37 +0530

    locationRequest - getLast location

--- app/src/main/java/com/sagar/real_time_notification/MainActivity.java
+++ app/src/main/java/com/sagar/real_time_notification/MainActivity.java
@@ -1,8 +1,11 @@
 package com.sagar.real_time_notification;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
@@ -95,7 +98,20 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "GoogleApiClient onConnected");
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000); // location update time
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                Log.i(TAG, "Last Location : " + mLastLocation.toString());
+                //updateUI(mLastLocation);
+            }
+
+
+        }
     }
 
     @Override
