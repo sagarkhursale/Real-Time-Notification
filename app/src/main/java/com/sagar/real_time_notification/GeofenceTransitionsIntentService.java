c7fcc2f3f7a71d91907958a5332843c1438e987e
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-13 07:02:23 +0530

    geofence Error messages

--- app/src/main/java/com/sagar/real_time_notification/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/sagar/real_time_notification/GeofenceTransitionsIntentService.java
@@ -1,8 +1,17 @@
 package com.sagar.real_time_notification;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GeofenceTransitionsIntentService extends IntentService {
@@ -23,8 +32,28 @@ public class GeofenceTransitionsIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent != null && geofencingEvent.hasError()) {
+            String errorMessages = GeofenceErrorMessages.getErrorString(this, geofencingEvent.getErrorCode());
+            Log.e(TAG, errorMessages);
+        }
 
+        // get transition type
+        assert geofencingEvent != null;
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // get the geofences that were triggered..
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+        } else {
+            Log.e(TAG, getString(R.string.geofence_transition_invalid_type, geofenceTransition));
+        }
+        // end
     }
+
 
     // END
 }
