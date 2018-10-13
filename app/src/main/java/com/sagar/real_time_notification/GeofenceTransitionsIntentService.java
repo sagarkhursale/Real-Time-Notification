fdcd4a211b45250df99674806a7ba4371f61e528
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-13 07:12:04 +0530

    send Notification with transition detail

--- app/src/main/java/com/sagar/real_time_notification/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/sagar/real_time_notification/GeofenceTransitionsIntentService.java
@@ -1,9 +1,15 @@
 package com.sagar.real_time_notification;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -48,20 +54,22 @@ public class GeofenceTransitionsIntentService extends IntentService {
             // get the geofences that were triggered..
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
 
-            // get transition details as a string
+
             String geofenceTransitionDetail = getGeofenceTransitionDetails(
                     this,
                     geofenceTransition,
                     triggeringGeofences
             );
 
-
-
+            // send notification
+            sendNotification(geofenceTransitionDetail);
+            Log.i(TAG, geofenceTransitionDetail);
         } else {
             Log.e(TAG, getString(R.string.geofence_transition_invalid_type, geofenceTransition));
         }
         // end
     }
+
 
 
     private String getGeofenceTransitionDetails(Context context, int geofenceTransition,
@@ -81,6 +89,7 @@ public class GeofenceTransitionsIntentService extends IntentService {
     }
 
 
+
     private String getTransitionString(int transitionType) {
         switch (transitionType) {
             case Geofence.GEOFENCE_TRANSITION_ENTER:
@@ -92,6 +101,44 @@ public class GeofenceTransitionsIntentService extends IntentService {
         }
     }
 
+
+
+    private void sendNotification(String notificationDetails) {
+        //  Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // PendingIntent with entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // notification builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.ic_notification)
+                // In a real app, you may want to use a library like Volley
+                // to decode the Bitmap.
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                        R.drawable.ic_notification))
+                .setColor(Color.RED)
+                .setContentTitle(notificationDetails)
+                .setContentText(getString(R.string.geofence_transition_notification_text))
+                .setContentIntent(notificationPendingIntent);
+        builder.setAutoCancel(true);
+
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        assert mNotificationManager != null;
+        mNotificationManager.notify(0, builder.build());
+    }
 
 
     // END
